c01ba9cd18303a6cbbd8db2d6f0889e8883a5d4a
Arjun K V <arjunvijayan470@gmail.com>
2022-11-10 19:11:50 +0530

    added a simple chat functionality using socket.io

--- index.js
+++ index.js
@@ -37,29 +37,7 @@ app.use('/api/user', userRoutes)
 
 app.get('/api/userslist', (req, res) => {
     const usersList = [
-        { id: 1, userName: "user1" },
-        { id: 2, userName: "user2" },
-        { id: 3, userName: "user3" },
-        { id: 4, userName: "user4" },
-        { id: 5, userName: "user5" },
-        { id: 6, userName: "user6" },
-        { id: 7, userName: "user7" },
-        // { id: 8, userName: "user8" },
-        // { id: 9, userName: "user9" },
-        // { id: 10, userName: "user10" },
-        // { id: 11, userName: "user11" },
-        // { id: 12, userName: "user12" },
-        // { id: 13, userName: "user13" },
-        // { id: 14, userName: "user14" },
-        // { id: 15, userName: "user15" },
-        // { id: 16, userName: "user16" },
-        // { id: 17, userName: "user17" },
-        // { id: 18, userName: "user18" },
-        // { id: 19, userName: "user19" },
-        // { id: 20, userName: "user20" },
-        // { id: 21, userName: "user21" },
-        // { id: 22, userName: "user22" },
-        // { id: 23, userName: "user23" },
+        { id: 1, userName: "chatroom" },
     ]
 
     res.json({ usersList })
@@ -68,29 +46,7 @@ app.get('/api/userslist', (req, res) => {
 app.get('/api/user/:userId', (req, res) => {
     const { userId } = req.params
     const usersList = [
-        { id: 1, userName: "user1" },
-        { id: 2, userName: "user2" },
-        { id: 3, userName: "user3" },
-        { id: 4, userName: "user4" },
-        { id: 5, userName: "user5" },
-        { id: 6, userName: "user6" },
-        { id: 7, userName: "user7" },
-        { id: 8, userName: "user8" },
-        { id: 9, userName: "user9" },
-        { id: 10, userName: "user10" },
-        { id: 11, userName: "user11" },
-        { id: 12, userName: "user12" },
-        { id: 13, userName: "user13" },
-        { id: 14, userName: "user14" },
-        { id: 15, userName: "user15" },
-        { id: 16, userName: "user16" },
-        { id: 17, userName: "user17" },
-        { id: 18, userName: "user18" },
-        { id: 19, userName: "user19" },
-        { id: 20, userName: "user20" },
-        { id: 21, userName: "user21" },
-        { id: 22, userName: "user22" },
-        { id: 23, userName: "user23" },
+        { id: 1, userName: "chatroom" },
     ]
     const user = usersList.find(u => u.id == userId)
 
@@ -99,6 +55,9 @@ app.get('/api/user/:userId', (req, res) => {
 
 io.on('connection', socket => {
     console.log(`new user joined - ${socket.id}`)
+    socket.on('send_message', data => {
+        socket.broadcast.emit('receive_message', data)
+    })
 })
 
 app.use(errorHandler)
